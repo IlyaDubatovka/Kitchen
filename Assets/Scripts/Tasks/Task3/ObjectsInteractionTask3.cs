02d4774d69b5f9c6fbcd60152282021d6690fb8e
Ilya Dubatovka <ilya.dubatovka@mail.ru>
2024-03-10 20:32:06 +0300

    feat: enable/disable light on the button 2

--- Assets/Scripts/Tasks/Task3/ObjectsInteractionTask3.cs
+++ Assets/Scripts/Tasks/Task3/ObjectsInteractionTask3.cs
@@ -1,7 +1,23 @@
+using System;
 using UnityEngine;
 
 public class ObjectsInteractionTask3 : MonoBehaviour
 {
-    // TODO: Получите доступ ко всем объектам сцены со скриптом Lamp
-    // TODO: При нажатии на кнопку 2 на клавиатуре вызывайте у всех полученных объектов метод Interact
+    private Lamp[] lamps;
+
+    private void Start()
+    {
+        lamps = FindObjectsOfType<Lamp>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            for (var i = 0; i < lamps.Length; i++)
+            {
+                lamps[i].Interact();
+            }
+        }
+    }
 }
